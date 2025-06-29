2e3b1ace1e53819d355aff96100102440bb181e8
Fiqi <rorymercury337@gmail.com>
2025-06-29 07:13:01 +0700

    Navbar Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-petshop shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <i class="fas fa-paw"></i> {{ config('app.name', 'Petshop') }}
+                    <i class="fas fa-paw"></i> {{ config('CatLovers', 'Catlover Petshop') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
